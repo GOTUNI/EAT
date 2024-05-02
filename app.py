8df66ac0705bf5e525c97c4aa81d3b1a963252cf
GOTUNI <168701971+GOTUNI@users.noreply.github.com>
2024-05-02 23:05:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage
-from flex_message import generate_google_style_message
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage, FlexSendMessage
 import requests
 import random
 
 app = Flask(__name__)
 
-CHANNEL_ACCESS_TOKEN = 'ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU='
-CHANNEL_SECRET = '4226f38b9cd8bce4d0417d29d575f750'
-GOOGLE_MAPS_API_KEY = 'AIzaSyD5sX433QilH8IVyjPiIpqqzJAy_dZrLvE'
+CHANNEL_ACCESS_TOKEN = 'YOUR_CHANNEL_ACCESS_TOKEN'
+CHANNEL_SECRET = 'YOUR_CHANNEL_SECRET'
+GOOGLE_MAPS_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY'
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
+def search_nearby_restaurant(lat, lng):
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=1000&type=restaurant&key={GOOGLE_MAPS_API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    if 'results' in data and len(data['results']) > 0:
+        restaurant = random.choice(data['results'])
+        name = restaurant.get('name', 'Unknown')
+        address = restaurant.get('vicinity', 'Unknown')
+        map_url = f"https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={restaurant['place_id']}"
+        return {'name': name, 'address': address, 'map_url': map_url}
+    else:
+        return None
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    lat = event.message.latitude
+    lng = event.message.longitude
+    restaurant = search_nearby_restaurant(lat, lng)
+    if restaurant:
+        google_style_message = generate_google_style_message(restaurant)
+        line_bot_api.reply_message(event.reply_token, google_style_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="抱歉，附近沒有找到餐廳"))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -26,28 +49,43 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location_message(event):
-    lat = event.message.latitude
-    lng = event.message.longitude
-    restaurant = search_nearby_restaurant(lat, lng)
-    if restaurant:
-        reply_text = f"我找到一家附近的餐廳：{restaurant['name']}，地址：{restaurant['address']}"
-    else:
-        reply_text = "抱歉，附近沒有找到餐廳"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
-
-def search_nearby_restaurant(lat, lng):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=1000&type=restaurant&key={GOOGLE_MAPS_API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    if 'results' in data and len(data['results']) > 0:
-        restaurant = random.choice(data['results'])
-        name = restaurant.get('name', 'Unknown')
-        address = restaurant.get('vicinity', 'Unknown')
-        return {'name': name, 'address': address}
-    else:
-        return None
+def generate_google_style_message(restaurant):
+    return FlexSendMessage(
+        alt_text='Google Style Message',
+        contents={
+            "type": "bubble",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": restaurant['name'],
+                        "weight": "bold",
+                        "size": "xl",
+                        "margin": "md"
+                    },
+                    {
+                        "type": "text",
+                        "text": restaurant['address'],
+                        "wrap": True,
+                        "margin": "md"
+                    },
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "uri",
+                            "label": "查看地圖",
+                            "uri": restaurant['map_url']
+                        },
+                        "color": "#1AA260",
+                        "style": "primary",
+                        "margin": "md"
+                    }
+                ]
+            }
+        }
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
